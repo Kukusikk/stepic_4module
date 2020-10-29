786d5d406172ca8b52214a0e6f8578f29b138f7c
fuckingforest <lada.laikina@yandex.ru>
2020-10-29 04:11:32 +0300

    my result in stepic-selenium

--- test_product_page.py
+++ test_product_page.py
@@ -1,14 +1,10 @@
-from pages.product_page import ProductPage
-from pages.login_page import LoginPage
+from .pages.product_page import ProductPage
+from .pages.login_page import LoginPage
 import time
 import pytest
-from pages.basket_page import BasketPage
+from .pages.basket_page import BasketPage
 
-
-
-
-    
-
+@pytest.mark.skip
 def test_guest_cant_see_success_message_after_adding_product_to_basket(browser):
     link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
     page = ProductPage(browser, link)
@@ -33,6 +29,25 @@ def test_guest_can_go_to_login_page_from_product_page(browser):
     login_page.should_be_login_page()
 
 
+
+def test_message_disappeared_after_adding_product_to_basket(browser):
+    link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
+    page = ProductPage(browser, link)
+    page.open()
+    page.save_other_info()
+    page.should_not_be_success_message()
+
+
+@pytest.mark.need_review
+def test_guest_can_add_product_to_basket(browser):
+    link = "http://selenium1py.pythonanywhere.com/catalogue/the-shellcoders-handbook_209/?promo=newYear"
+    page = ProductPage(browser, link)
+    page.open()
+    page.save_other_info()
+    page.add_in_bucket()
+    page.should_be_product_page()
+
+@pytest.mark.need_review
 def test_guest_cant_see_product_in_basket_opened_from_product_page(browser):
     link = "http://selenium1py.pythonanywhere.com/en-gb/catalogue/the-city-and-the-stars_95/"
     page = ProductPage(browser, link)
@@ -43,14 +58,7 @@ def test_guest_cant_see_product_in_basket_opened_from_product_page(browser):
     page.bucket_is_empty_text()
     page.bucket_is_empty_text()
 
-def test_message_disappeared_after_adding_product_to_basket(browser):
-    link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
-    page = ProductPage(browser, link)
-    page.open()
-    page.save_other_info()
-    page.should_not_be_success_message()
-
-@pytest.mark.regitration_and_login_user
+@pytest.mark.need_review
 class TestUserAddToBasketFromProductPage():
     @pytest.fixture(scope="function", autouse=True)
     def setup(self, browser):
@@ -84,7 +92,7 @@ class TestUserAddToBasketFromProductPage():
     #                               "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer7",
     #                               "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer8",
     #                               "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer9"])
-    def test_guest_can_add_product_to_basket(self, browser):
+    def test_user_can_add_product_to_basket(self, browser):
         link = "http://selenium1py.pythonanywhere.com/catalogue/the-shellcoders-handbook_209/?promo=newYear"
         page = ProductPage(browser, link)
         page.open()
